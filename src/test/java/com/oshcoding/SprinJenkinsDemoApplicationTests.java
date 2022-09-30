8654f7bbc2972acb2d701a5608c44a304fe1b1c9
oshan-sameera <oshan951@gmail.com>
2022-09-30 15:49:56 +1000

    test is failed

--- src/test/java/com/oshcoding/SprinJenkinsDemoApplicationTests.java
+++ src/test/java/com/oshcoding/SprinJenkinsDemoApplicationTests.java
@@ -12,12 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SprinJenkinsDemoApplicationTests {
 
 	
-	//public static Logger logger = LoggerFactory.getLogger(SprinJenkinsDemoApplication.class);
+	public static Logger logger = LoggerFactory.getLogger(SprinJenkinsDemoApplication.class);
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing ....");
 		logger.info("Test case executing second log statement....");
-		assertEquals(true, true);
+		assertEquals(true, false);
 	}
 
 }
